43f4593d1460eb85fe0f2949618f6f3b71d3c7e9
Manuel Forcén Muñoz <manuforcen@gmail.com>
2021-10-06 20:59:50 +0200

    OS init routine completed Context switch routine started Global objects created

---
+++ os/asm.s
@@ -0,0 +1,73 @@
+.thumb
+.syntax unified
+
+.global switch_to_psp
+.type switch_to_psp,%function
+
+.global load_context
+.type load_context,%function
+
+.global save_context
+.type save_context,%function
+
+.global init_os
+.type init_os,%function
+
+.global raise_svc
+.type raise_svc,%function
+
+.global SVCall
+.type SVCall,%function
+
+SVCall:
+	mrs r0, psp @; r0 is saved automatically when entering the handler
+	ldmia r0!, {r4, r11} @; saving the rest of the cpu registers into the stack pointer
+	ldr r1, =GLOBAL_SP
+	ldr r2, =NOW_TASK
+	lsl r2, r2, #2 @; left shift by 2 in order to get address of specific task
+	add r1, r1, r2 @; now address is in r0
+	str r0, [r1] @; r0 contains psp, r1 contains pointer to global_sp item
+	bx lr
+	.size SVCall, . - SVCall @; . is the this address operator, like the linker script
+
+
+switch_to_psp:
+	ldr r0,=0x00000003
+	msr control,r0
+	isb
+	bx lr
+	.size switch_to_psp, . - switch_to_psp
+	
+
+load_context:
+	ldmia	r0!, {r4,r11} @; this will read all the registers from r0, which is the stack pointer of the task
+	bx	lr @; this will return to the caller
+	.size load_context, . - load_context
+
+
+save_context: @; this operation will take all the registers that the CPU did not store and save it to recover it later
+	mrs	r0, psp @; read process stack pointer address and place it in r0
+	stmdb	r0!, {r4-r11} @; write several registers at once (store multiple decrement before), due to !, r0 new address will be updated
+	bx	lr @; this will return to the caller
+	.size save_context, . - save_context
+
+
+init_os: @;this function will perform the first context change
+	ldr r0, =GLOBAL_SP
+	ldr r0, [r0]
+	msr psp, r0
+	ldr r1,=0x00000003
+	msr control, r1
+	isb
+	add r0, r0, #56
+	ldr r0, [r0]
+	bx r0
+	@;bx lr
+	.size init_os, . - init_os
+
+
+
+raise_svc:
+	svc 1
+	bx lr
+	.size raise_svc, . - raise_svc
--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,56 @@ extern "C" fn Hola() {
 	return;
 }
 
+
+#[no_mangle]
+static mut NOW_TASK: u8 = 0;
+#[no_mangle]
+static mut NUM_TASKS: u8 = 0;
+#[no_mangle]
+static mut GLOBAL_SP : [*mut u32;4] = [0 as *mut u32;4];
+//#[no_mangle]
+//static mut GLOBAL_NAMES : [[u8;32];4] = [[0;32];4];
+#[no_mangle]
+static mut GLOBAL_STACKS : [[u64;512];4] = [[0;512];4]; //8 byte aligned memory 4kb spaces
+
+fn task_1() {
+	loop {
+		asm::nop();
+	}
+}
+
+extern "C" {
+	fn switch_to_psp();
+	fn load_context();
+	fn save_context();
+	fn raise_svc();
+	fn init_os();
+}
+
+fn create_task(fn_ptr: *const ()) {
+	unsafe {
+		let this_task = NUM_TASKS as usize;
+		let this_stack = GLOBAL_STACKS[this_task].as_mut_ptr() as *mut u32;
+		NUM_TASKS += 1;
+		GLOBAL_SP[this_task] = this_stack.offset(2048 - 16); // first stack level should be created
+		*this_stack.offset(2047) = 0x01000000;
+		*this_stack.offset(2046) = fn_ptr as u32;
+	}
+}
+
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+	unsafe {
+		NOW_TASK = 0;
+		NUM_TASKS = 0;
+	}
+	
+	create_task(task_1 as *const ());
+
+	unsafe {
+		init_os();
+	}
+    
 	loop {
         // your code goes here
     }
