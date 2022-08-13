53701ce7dd3afd9415b05515e90f532ecaf9c67b
Manuel Forcén Muñoz <manuforcen@gmail.com>
2022-08-13 10:51:27 +0200

    Added volatile cell to stack variables

--- src/os/stack.rs
+++ src/os/stack.rs
@@ -1,11 +1,13 @@
+use super::utils::VolatileCell;
+
 #[repr(C)]
 pub struct HardStack{
-	pub r0: u32,
-	pub r1: u32,
-	pub r2: u32,
-	pub r3: u32,
-	pub r12: u32,
-	pub lr: u32,
-	pub pc: u32,
-	pub xpsr: u32
+	pub r0: VolatileCell<u32>,
+	pub r1: VolatileCell<u32>,
+	pub r2: VolatileCell<u32>,
+	pub r3: VolatileCell<u32>,
+	pub r12: VolatileCell<u32>,
+	pub lr: VolatileCell<u32>,
+	pub pc: VolatileCell<u32>,
+	pub xpsr: VolatileCell<u32>
 }
